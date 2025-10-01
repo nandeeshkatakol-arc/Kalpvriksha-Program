abd2d13f0c52e53025c734858bd79c839490c547
Nandeesh <nandeeshkatakol@gmail.com>
2025-10-01 21:31:36 +0530

    Added functionality

---
+++ CRUD.c
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define FILEname "users.txt"
+
+void Create();
+void Read();
+void Update();
+void Delete();
+
+struct User
+{
+    char name[40];
+    int id;
+    int age;
+};
+
+
+int main()
+{
+    int choice;
+    
+    while(1)
+    {
+        printf("\n1. Create\n2. Read\n3. Update\n4. Delete\n5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice)
+        {
+            case 1:
+                Create();
+                break;
+            case 2:
+                Read();
+                break;
+            case 3:
+                Update();
+                break;
+            case 4:
+                Delete();
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("***Invalid choice!***\n");
+        }
+    }
+    return 0;
+}
+
+void Create()
+{
+    FILE *fp =fopen(FILEname, "a+");
+
+    printf("Enter user's details");
+
+    struct User user;
+    printf("1.ID : ");
+    scanf("%d", &user.id);
+    printf("2.Name : ");
+    scanf("%s", user.name);
+    printf("3.Age : "); 
+    scanf("%d", &user.age);
+
+    printf("User created successfully!\n");
+    fclose(fp);
+
+}
+
+void Read()
+{
+    FILE *fp = foppen(FILEname, "r");
+    if(!fp)
+    {
+        printf("User info doesn't exists\n");
+        return;
+    }
+    while(fscanf(fp,"%d %s %d", &user.id, user.name, &user.age) == 3)
+    {
+        printf("ID: %d, Name: %s, Age: %d\n", user.id, user.name, user.age);
+    }
+
+    fclose(fp);
+
+}
+
+void Update()
+{
+    FILE *fp = fopen(FILEname, "r");
+    if(!fp)
+    {
+        printf("User info doesn't exists\n");
+        return;
+    }
+
+    int id,flag=0;
+    printf("Enter user ID to update: ");
+    scanf("%d", &id);
+
+    FILE *temp=fopen("temp.txt","w");
+    struct User user;
+    while(fscanf(fp,"%d %s %d", &user.id,user.name,%user.age)!=EOF)
+    {
+        if(user.id==id)
+        {
+            flag=1;
+
+            printf("Enter new details\n"); 
+            printf("2.Name : ");
+            scanf("%s", user.name);
+            printf("3.Age : "); 
+            scanf("%d", &user.age);
+        }
+    }
+    fclose(fp);
+    fclose(temp);
+    remove(FILEname);
+    rename("temp.txt",FILEname);
+
+    if(flag==1)
+    {
+        printf("User info updated successfully!\n");
+
+    }
+    else
+    {
+        printf("User with ID %d not found!\n", id);
+    }
+
+}
+
+void delete()
+{
+    FILE *fp=fopen(FILEname,"r");
+    if(!fp)
+    {
+        printf("User info doesn't exists\n");
+        return;
+    }
+    int 
+}
