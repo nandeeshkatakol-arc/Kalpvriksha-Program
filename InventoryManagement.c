ad15cfa074e4be3c246e885e1fe62d454cf0fac8
Nandeesh <nandeeshkatakol@gmail.com>
2025-11-03 20:44:16 +0530

    made suggested changes by Tanishka

--- InventoryManagement.c
+++ InventoryManagement.c
@@ -26,21 +26,99 @@ enum MenuOption
 product *inventory = NULL;
 int totalProductCount = 0;
 
-void readProduct(product *item)
+bool isDuplicateID(int ID, int currentIndex)
 {
-    printf("Proct ID: ");
-    scanf("%d",&item->ID);
-    getchar();
-    printf("Product Name: ");
-    fgets(item->name, sizeof(item->name), stdin);
-    item->name[strcspn(item->name, "\n")] = '\0';
-    printf("Price: ");
-    scanf("%f",&item->price);
-    printf("Quantity: ");
-    scanf("%d",&item->quantity);
+    for (int i = 0; i < currentIndex; i++)
+    {
+        if (inventory[i].ID == ID)
+            return true;
+    }
+    return false;
 }
 
-void show_menu() {
+bool isDuplicateName(const char *name, int currentIndex)
+{
+    for (int i = 0; i < currentIndex; i++)
+    {
+        if (strcmp(inventory[i].name, name) == 0)
+            return true;
+    }
+    return false;
+}
+
+void readProduct(product *item, int currentIndex)
+{
+    while (1)
+    {
+        printf("Product ID: ");
+        scanf("%d", &item->ID);
+        getchar();
+
+        if (item->ID < 1 || item->ID > 10000)
+        {
+            printf("Error: Product ID must be between 1 and 10000.\n");
+        }
+        else if (isDuplicateID(item->ID, currentIndex))
+        {
+            printf("Error: Product ID already exists. Try another ID.\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    while (1)
+    {
+        printf("Product Name: ");
+        fgets(item->name, sizeof(item->name), stdin);
+        item->name[strcspn(item->name, "\n")] = '\0';
+
+        int nameLength = strlen(item->name);
+        if (nameLength < 1 || nameLength > 50)
+        {
+            printf("Error: Product name length must be between 1 and 50 characters.\n");
+        }
+        else if (isDuplicateName(item->name, currentIndex))
+        {
+            printf("Error: Product name already exists. Enter a different name.\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    while (1)
+    {
+        printf("Price: ");
+        scanf("%f", &item->price);
+        if (item->price < 0 || item->price > 100000)
+        {
+            printf("Error: Price must be between 0 and 100000.\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    while (1)
+    {
+        printf("Quantity: ");
+        scanf("%d", &item->quantity);
+        if (item->quantity < 0 || item->quantity > 1000000)
+        {
+            printf("Error: Quantity must be between 0 and 1000000.\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+void showMenu() {
     printf("\n========= INVENTORY MENU =========\n");
     printf("1. Add New Product\n");
     printf("2. View All Products\n");
@@ -64,7 +142,7 @@ void addProduct()
     inventory = temp;
 
     printf("\nEnter details for new product:\n");
-    readProduct(&inventory[totalProductCount]);
+    readProduct(&inventory[totalProductCount], totalProductCount);
     totalProductCount++;
     printf("Product added successfully.\n");
 }
@@ -251,44 +329,52 @@ int main()
         for (int i = 0; i < totalProductCount; i++)
         {
             printf("\nEnter details for product %d:\n", i + 1);
-            readProduct(&inventory[i]);
+            readProduct(&inventory[i], i);
         }
     }
 
     do
     {
-        show_menu();
+        showMenu();
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
-        case ADD_PRODUCT:
-            addProduct();
-            break;
-        case VIEW_PRODUCTS:
-            viewProduct();
-            break;
-        case UPDATE_QUANTITY:
-            updateQuantity();
-            break;
-        case SEARCH_BY_ID:
-            searchByID();
-            break;
-        case SEARCH_BY_NAME:
-            search_by_name();
-            break;
-        case SEARCH_BY_PRICE_RANGE:
-            search_by_price();
-            break;
-        case DELETE_PRODUCT:
-            deleteProduct();
-            break;
-        case EXIT_PROGRAM:
-            printf("Releasing allocated memory and exiting program.\n");
-            break;
-        default:
-            printf("Invalid choice. Enter a number between 1 and 8.\n");
+            case ADD_PRODUCT:
+                addProduct();
+                break;
+
+            case VIEW_PRODUCTS:
+                viewProduct();
+                break;
+
+            case UPDATE_QUANTITY:
+                updateQuantity();
+                break;
+
+            case SEARCH_BY_ID:
+                searchByID();
+                break;
+
+            case SEARCH_BY_NAME:
+                search_by_name();
+                break;
+
+            case SEARCH_BY_PRICE_RANGE:
+                search_by_price();
+                break;
+
+            case DELETE_PRODUCT:
+                deleteProduct();
+                break;
+
+            case EXIT_PROGRAM:
+                printf("Releasing allocated memory and exiting program.\n");
+                break;
+
+            default:
+                printf("Invalid choice. Enter a number between 1 and 8.\n");
         }
 
     } while (choice != EXIT_PROGRAM);
